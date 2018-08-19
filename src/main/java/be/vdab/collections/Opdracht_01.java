3f15536efc7f8ed8543799bb38186898f8e17cf4
Jimmy <Jimmy@PC-JIMMY.home>
2018-08-19 14:51:47 +0200

    eerste commit

---
+++ src/main/java/be/vdab/collections/Opdracht_01.java
@@ -0,0 +1,13 @@
+package be.vdab.collections;
+
+/**
+ * Hello world!
+ *
+ */
+public class Opdracht_01 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
